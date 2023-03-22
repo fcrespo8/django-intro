afd6a2f37fcb84e978abf496ab81a2e902e1ffed
Francisco Crespo <fcrespo8@hotmail.com>
2023-03-22 10:57:12 +0100

    programador model created

--- tutorial3/pruebadb/admin.py
+++ tutorial3/pruebadb/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Empresa
+from .models import Empresa, Programador
 
 # Register your models here.
 
 admin.site.register(Empresa)
+admin.site.register(Programador)
