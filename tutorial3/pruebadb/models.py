b0ac962c1e1520ef4ccd997ac101ad700dd9e1e4
Francisco Crespo <fcrespo8@hotmail.com>
2023-03-22 14:22:54 +0100

    final commit tutorial 3

--- tutorial3/pruebadb/models.py
+++ tutorial3/pruebadb/models.py
@@ -8,19 +8,19 @@ class Empresa(models.Model):
 	def __str__(self):
 		return self.nombre
 
-# class Lenguaje(models.Model):
-# 	nombre = models.CharField(max_length=40)
-# 	creador = models.CharField(max_length=40, null=True)
+class Lenguaje(models.Model):
+	nombre = models.CharField(max_length=40)
+	creador = models.CharField(max_length=40, null=True)
 
-# 	def __str__(self):
-# 		return self.nombre
+	def __str__(self):
+		return self.nombre
 
 class Programador(models.Model):
 	nombre = models.CharField(max_length=40)
-	# edad = models.IntegerField(null=True)
+	edad = models.IntegerField(null=True)
 	empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE,
 											related_name="empleados")
-	# lenguaje = models.ManyToManyField(Lenguaje)
+	lenguaje = models.ManyToManyField(Lenguaje)
 
 	def __str__(self):
 		return self.nombre
