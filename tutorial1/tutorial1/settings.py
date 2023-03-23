f57a9afe57f1d7c1138761ace87ea77f54a895ea
Francisco <104527931+fcrespo8@users.noreply.github.com>
2023-03-23 10:09:09 +0100

    Update settings.py with no secret key

--- tutorial1/tutorial1/settings.py
+++ tutorial1/tutorial1/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mn=a)r@87p0edus2h5_u1fzvq5w0=9rs5=&+sjt@*9@4^v(a01'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
